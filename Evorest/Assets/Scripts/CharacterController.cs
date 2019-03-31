1948c5874740826b1a76dc96f4648c6a22633453
mrmakroefi <mrm_int_main@yahoo.com>
2019-04-01 01:49:42 +0700

    simple movement

---
+++ Evorest/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour {
+
+    private Rigidbody2D rb2D;
+
+    public float maxSpeed = 2f;
+    public float jumpHeight = 4f;
+    public float agility = 0.2f;
+
+    private Vector2 currentVelocity;
+	void Awake () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+    public void Move(float velocity)
+    {
+        float targetSmoothTime = Mathf.Abs(velocity) > 0 ? 0.04f : agility;
+        Vector2 targetVelocity = new Vector2(velocity, rb2D.velocity.y);
+        rb2D.velocity = Vector2.SmoothDamp(rb2D.velocity, targetVelocity, ref currentVelocity, targetSmoothTime);
+        rb2D.velocity = new Vector2(Mathf.Clamp(rb2D.velocity.x, -maxSpeed, maxSpeed), rb2D.velocity.y);
+    }
+
+    protected void Jump()
+    {
+        rb2D.velocity = new Vector2(rb2D.velocity.x, jumpHeight);
+    }
+
+}
---
+++ Evorest/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : CharacterController {
+
+    private float horizontalInput;
+    private bool jumpInput;
+    
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        
+        if (!jumpInput) {
+            jumpInput = Input.GetKeyDown(KeyCode.Space);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float targetVelocity = horizontalInput * maxSpeed;
+        Move(targetVelocity);
+        if (jumpInput) {
+            Jump();
+            jumpInput = false;
+        }
+    }
+
+}
